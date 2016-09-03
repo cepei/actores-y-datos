294b3ac21c75734689c4e92e33eafdc71917670c
Emmanuel Ariza <ejariza@gmail.com>
2016-09-02 21:06:49 -0500

    Update 2016-09-03T02:06:45.829Z

--- countries/countries.js
+++ countries/countries.js
@@ -1,14 +1,22 @@
 'use strict';
 
 angular.module('myApp.countries', ['ngRoute'])
-
+.controller('CountriesCtrl', [function() {
+	console.log("Countries")
+}])
+.controller('CountryCtrl', ["$scope", "$routeParams", function($scope, $routeParams) {
+	console.log($routeParams)
+}])
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/countries', {
-    templateUrl: 'countries/countries.html',
-    controller: 'CountriesCtrl'
-  });
+  $routeProvider
+  	.when('/countries', {
+    	templateUrl: 'countries/countries.html',
+    	controller: 'CountriesCtrl'
+  	})
+  	.when('/countries/:countryId', {
+    	templateUrl: 'countries/country.html',
+    	controller: 'CountryCtrl'	
+  	});
 }])
 
-.controller('CountriesCtrl', [function() {
-
-}]);
+;
