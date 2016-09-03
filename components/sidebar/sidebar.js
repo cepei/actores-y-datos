4998aa19c1cc42c83f8ce9196bf34c2823093178
Emmanuel Ariza <ejariza@gmail.com>
2016-09-03 11:06:40 -0500

    Update 2016-09-03T16:06:34.981Z

--- components/sidebar/sidebar.js
+++ components/sidebar/sidebar.js
@@ -2,12 +2,17 @@
 
 // ............... NOT TESTED
 angular.module('myApp.sidebar',['ngRoute'])
-.controller('SidebarCtrl', ['$location', '$scope' ,function($location, $scope) {
+.controller('SidebarCtrl', ['$location', '$scope' , "$routeParams", function($location, $scope, $routeParams) {
 	
 	$scope.path = $location.path();
 	$scope.$on('$routeChangeStart', function(next, current) { 
+		// console.log(current)
 		$scope.path = $location.path();
+		$scope.countryId = current.params.countryId;
+		// console.log($routeParams)
  });
+
+	
 
 }])
 .directive("sidebar", function(){
--- countries/countries.js
+++ countries/countries.js
@@ -2,10 +2,43 @@
 
 angular.module('myApp.countries', ['ngRoute'])
 .controller('CountriesCtrl', [function() {
-	console.log("Countries")
+
 }])
-.controller('CountryCtrl', ["$scope", "$routeParams", function($scope, $routeParams) {
-	console.log($routeParams)
+.controller('CountryCtrl', ["$scope", "$routeParams", "$http",function($scope, $routeParams, $http) {
+
+	function getStructure(country){
+		var structure = {"head":[],"list":[]}
+		country["Composición de la estructura institucional"]
+				.split("\n")
+				.forEach(function(elem){
+					if(elem.startsWith("-"))
+						structure.list.push(elem.substring(1,elem.length));
+					else{
+							if(elem.toLowerCase().indexOf(country["País"].toLowerCase()) == -1 && elem!="")
+								structure.head.push(elem);
+					}
+				})
+		console.log(structure);
+		return structure
+	}
+
+	$http.get("/countries/countries.csv").then(function(response){
+
+		var country = d3.csv.parse(response.data)
+						.find(function(country){
+								return country.id == $routeParams.countryId;
+							});
+		console.log(country);
+		$scope.name = country["País"];
+		$scope.pib = country["PIB (Miles de Millones)"];
+		$scope.incomeLevel =  country["Nivel de renta"];
+		$scope.population = country["Población (Millones)"];
+		$scope.structure = getStructure(country);
+		$scope.incidenceOfPoverty = country["Tasa de incidencia de la pobreza sobre la base de la línea nacional"];
+		$scope.hdi = country["Índice de Desarrollo Humano"];
+		$scope.hdiRank = country["Posición en el Indice de Desarrollo Humano"];
+		$scope.actions = country["Acciones implementadas"];
+		});		
 }])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
@@ -17,6 +50,4 @@ angular.module('myApp.countries', ['ngRoute'])
     	templateUrl: 'countries/country.html',
     	controller: 'CountryCtrl'	
   	});
-}])
-
-;
+}]);
